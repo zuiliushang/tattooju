db195b39b27a071aaaa91e5cc71fc765dc61b063
Raindrops <l>
2018-05-20 23:13:35 +0800

    update config

--- code/src/main/java/com/tattooju/TattoojuApp.java
+++ code/src/main/java/com/tattooju/TattoojuApp.java
@@ -3,7 +3,10 @@ package com.tattooju;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import tk.mybatis.spring.annotation.MapperScan;
+
 @SpringBootApplication
+@MapperScan("com.tattooju")
 public class TattoojuApp {
 
 	public static void main(String[] args) {
--- code/src/main/java/com/tattooju/ctrl/SystemCtrl.java
+++ code/src/main/java/com/tattooju/ctrl/SystemCtrl.java
@@ -2,8 +2,10 @@ package com.tattooju.ctrl;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/sys/")
+@RestController
 public class SystemCtrl {
 
 	@RequestMapping("greeting")
